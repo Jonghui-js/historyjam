99932b8397d1a88e5add6ace1c6c05c667f36422
jonghui-js <jonghui.js@gmail.com>
2020-04-30 22:13:34 +0900

    items ->cards

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Item from './Item';
 import { Loader } from 'semantic-ui-react';
+import { Card, Image, Divider } from 'semantic-ui-react';
 
 const Cards = ({ location }) => {
   const [cards, setCards] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     const fetchData = async () => {
       const res = await axios.get(`/search${location.search}`);
       setCards(res.data.currentAge);
@@ -36,12 +38,21 @@ const Cards = ({ location }) => {
   ) : (
     <div className='card-container'>
       {cards.map((card) => (
-        <Item
-          year={card.year}
-          name={card.name}
-          des={card.description}
-          key={card._id}
-        />
+        <Card fluid>
+          <Card.Content>
+            <h3>
+              {card.name} [{card.year}~]
+            </h3>
+            <Divider />
+
+            <Image
+              size='small'
+              floated='left'
+              src={`https://koreanhistoryjam.s3.ap-northeast-2.amazonaws.com/${card.year}.jpg`}
+            />
+            <p style={{ textAlign: 'left' }}>{card.description}</p>
+          </Card.Content>
+        </Card>
       ))}
     </div>
   );
